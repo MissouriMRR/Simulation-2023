8191be69cf490dca44404ae2a0e240874b5b7bf9
dirkwind <dirkwind707@gmail.com>
2022-12-01 18:56:17 -0600

    fix error with relative path, restructure code

--- scripts/run-servers.ps1
+++ scripts/run-servers.ps1
@@ -1,16 +1,36 @@
-$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+# This script runs PX4 and MavSDK at the same time
 
-if (-not(Test-Path -Path $scriptPath\server-config.json -PathType Leaf)) {
-    echo "config file not present; run setup.bat and fill out the information in the created json file"
+# Function that starts a new powershell instance with a given window name and command
+function New-Powershell {
+    param (
+        $name,
+        $command
+    )
+    return Start-Process powershell -ArgumentList "-noexit", "-noprofile", "-command", "`$host.ui.RawUI.WindowTitle = '$name'; $command"
+}
+
+# Get directory of this script and config file
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$configPath = "$scriptPath\server-config.json"
+
+# check if config file is present
+if (-not(Test-Path -Path $configPath -PathType Leaf)) {
+    Write-Output "config file not present; run setup.bat and fill out the information in the created json file"
     break
 }
 
-$config = Get-Content .\server-config.json | ConvertFrom-Json
-$autorun = $config.px4_path + '\autorun.bat'
+# parse config file
+$config = Get-Content $configPath | ConvertFrom-Json
 
+# format some config-related info, which will be used more than once
+$autorun = "$($config.px4_path)\autorun.bat"
+$drone_uri = "udp://:$($config.drone_port)"
+
+# create the modifed run-console.bat in the PX4 folder if it's missing
 if (-not(Test-Path -Path $autorun -PathType Leaf)) {
     Get-Content $scriptPath\templates\autorun.txt | Out-File -FilePath $autorun -Encoding oem
 }
 
-invoke-expression "cmd /c start powershell -noexit -Command 'cd $($config.px4_path); .\autorun.bat'"
-invoke-expression "cmd /c start powershell -noexit -Command 'cd $($config.mavsdk_server_path); .\mavsdk_server_bin.exe udp://:$($config.drone_port)'"
+# run PX4 and the MavSDK server
+New-Powershell "PX4" "cd $($config.px4_path); .\autorun.bat"
+New-Powershell "MavSDK Server ($drone_uri)" "cd $($config.mavsdk_server_path); .\mavsdk_server_bin.exe $drone_uri" 
