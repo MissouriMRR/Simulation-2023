47d185a02f767146bf62d3701b65bb0728552e87
dirkwind <dirkwind707@gmail.com>
2023-02-16 19:03:29 -0600

    add script that makes changing airsim settings trivial

---
+++ scripts/update_settings.bat
@@ -0,0 +1,6 @@
+@REM This script copies the content of the provided file into AirSim's settings
+
+@echo off&setlocal
+for %%i in ("%~dp0.") do set "folder=%%~fi"
+
+type %1 > %UserProfile%\Documents\AirSim\settings.json
