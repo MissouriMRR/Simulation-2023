15d71fb0d2bcd096ca47df960ec80dd269acee8a
heitmeih <harry.heitmeier@gmail.com>
2023-09-19 19:36:22 -0500

    update scripts to ps1 from bat

---
+++ scripts/setup.ps1
@@ -0,0 +1,10 @@
+# server-config.json will be gitignored
+# this file is to ensure everyone easily has the correct server-config.json format in the correct location
+
+$docs = [Environment]::GetFolderPath('Personal')
+
+Get-Content "$PSScriptRoot\templates\server-config.lock.json" > "$PSScriptRoot\server-config.json"
+Get-Content "$PSScriptRoot\templates\airsim_settings.lock.json" > "$docs\AirSim\settings.json"
+Get-Content "$PSScriptRoot\templates\airsim_settings.lock.json" > "$PSScriptRoot\airsim-settings.json"
+
+Write-Output "Setup complete"
---
+++ scripts/update_settings.ps1
@@ -0,0 +1,15 @@
+# This script copies the content of the provided file into AirSim's settings
+param(
+    [string]$file
+)
+
+$docs = [Environment]::GetFolderPath('Personal')
+$settings = "$PSScriptRoot\airsim-settings.json"
+
+if ($PSBoundParameters.ContainsKey('file')) {
+    $settings = "$file"
+}
+
+Get-Content "$settings" > "$docs\AirSim\settings.json"
+
+Write-Output "Settings updated!"
